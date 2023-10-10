ecda391fea4092dbb6a451191deb475f887210f8
Levente Oláh <levente@stud.uni-obuda.hu>
2023-10-10 11:57:55 +0200

    Add project files.

---
+++ A3C6TV_HFT_2023241.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A3C6TV_HFT_2023241.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
