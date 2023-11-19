2f22c6cac8c51340c06b0ef05d0e15e210d23425
Oláh Levente <levente@stud.uni-obuda.hu>
2023-11-19 10:40:41 +0100

    messing with the endpoint

--- A3C6TV_HFT_2023241.Endpoint/Startup.cs
+++ A3C6TV_HFT_2023241.Endpoint/Startup.cs
@@ -1,63 +1,41 @@
-using A3C6TV_HFT_2023241.Logic;
-using A3C6TV_HFT_2023241.Models;
 using A3C6TV_HFT_2023241.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace A3C6TV_HFT_2023241.Endpoint
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddTransient<TajfunDBContext>();
 
-            services.AddTransient<IRepository<Booking>, BookingRepository>();
-            services.AddTransient<IRepository<Customer>, CustomerRepository>();
-            services.AddTransient<IRepository<PoolTable>, PoolTableRepository>();
-
-            services.AddTransient<IBookingLogic, BookingLogic>();
-            services.AddTransient<ICustomerLogic, CustomerLogic>();
-            services.AddTransient<IPoolTableLogic, PoolTableLogic>();
 
 
 
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "A3C6TV_HFT_2023241.Endpoint", Version = "v1" });
-            });
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "A3C6TV_HFT_2023241.Endpoint v1"));
             }
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
             });
         }
     }
