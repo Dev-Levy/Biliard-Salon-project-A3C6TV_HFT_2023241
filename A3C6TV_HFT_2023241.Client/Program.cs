9660a3c6518a5aec4a57df5dad38ff24b6fc312b
Oláh Levente <levente@stud.uni-obuda.hu>
2023-11-22 17:16:34 +0100

    client layer started

--- A3C6TV_HFT_2023241.Client/Program.cs
+++ A3C6TV_HFT_2023241.Client/Program.cs
@@ -1,40 +1,98 @@
-﻿using A3C6TV_HFT_2023241.Logic;
-using A3C6TV_HFT_2023241.Repository;
+﻿using ConsoleTools;
+using MovieDbApp.RestClient;
+using System.Collections.Generic;
 using System;
 
 namespace A3C6TV_HFT_2023241.Client
 {
     internal class Program
     {
+        static RestService rest;
+
+        //ki kell dolgozni az összes crud metódust
+        static void Create(string entity)
+        {
+            if (entity == "Bookings")
+            {
+
+            }
+            else if (entity == "Customers")
+            {
+            }
+            else if(entity == "PoolTables")
+            {
+            }
+        }
+        static void List(string entity)
+        {
+            if (entity == "Bookings")
+            {
+            }
+            else if (entity == "Customers")
+            {
+            }
+            else if (entity == "PoolTables")
+            {
+            }
+            Console.ReadLine();
+        }
+        static void Update(string entity)
+        {
+            if (entity == "Bookings")
+            {
+            }
+            else if (entity == "Customers")
+            {
+            }
+            else if (entity == "PoolTables")
+            {
+            }
+        }
+        static void Delete(string entity)
+        {
+            if (entity == "Bookings")
+            {
+            }
+            else if (entity == "Customers")
+            {
+            }
+            else if (entity == "PoolTables")
+            {
+            }
+        }
         static void Main(string[] args)
         {
-            try
-            {
-                var ctx = new TajfunDBContext();
+            RestService rest = new RestService("http://localhost:7332/", "booking");
 
-                var bookingRepo = new BookingRepository(ctx);
-                var customerRepo = new CustomerRepository(ctx);
-                var pooltableRepo = new PoolTableRepository(ctx);
+            var bookingSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Bookings"))
+                .Add("Create", () => Create("Bookings"))
+                .Add("Delete", () => Delete("Bookings"))
+                .Add("Update", () => Update("Bookings"))
+                .Add("Exit", ConsoleMenu.Close);
 
-                var bookingLogic = new BookingLogic(bookingRepo);
-                var customerLogic = new CustomerLogic(customerRepo);
-                var pooltableLogic = new PoolTableLogic(pooltableRepo);
+            var customerSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Customers"))
+                .Add("Create", () => Create("Customers"))
+                .Add("Delete", () => Delete("Customers"))
+                .Add("Update", () => Update("Customers"))
+                .Add("Exit", ConsoleMenu.Close);
+
+            var pooltableSubManu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("PoolTables"))
+                .Add("Create", () => Create("PoolTables"))
+                .Add("Delete", () => Delete("PoolTables"))
+                .Add("Update", () => Update("PoolTables"))
+                .Add("Exit", ConsoleMenu.Close);
 
 
+            var menu = new ConsoleMenu(args, level: 0)
+                .Add("Bookings", () => bookingSubMenu.Show())
+                .Add("Customers", () => customerSubMenu.Show())
+                .Add("PoolTables", () => pooltableSubManu.Show())
+                .Add("Exit", ConsoleMenu.Close);
 
-                DateTime start = new DateTime(2023, 11, 20, 10, 00, 00);
-                DateTime end = new DateTime(2023, 12, 1, 20, 00, 00);
-
-                var frequentcust = bookingLogic.MostFrequentCustomers(5);
-                var number = bookingLogic.HowManyBookingsBetweenTwoDates(start, end);
-                var b_list = bookingLogic.BookingsBetweenTwoDates(start, end);
-                var table = bookingLogic.MostUsedTable();
-                var rate = bookingLogic.TablekindsBooked(start, end);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            menu.Show();
         }
     }
 }
