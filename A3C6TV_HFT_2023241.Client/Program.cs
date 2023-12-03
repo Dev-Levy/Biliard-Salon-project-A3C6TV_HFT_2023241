7d8556289cfc072c08e9d224c66316ead8897ab7
Oláh Levente <levente@stud.uni-obuda.hu>
2023-12-03 20:01:31 +0100

    kis hibakezelés aka try-catch mindenhova

--- A3C6TV_HFT_2023241.Client/Program.cs
+++ A3C6TV_HFT_2023241.Client/Program.cs
@@ -12,40 +12,49 @@ namespace A3C6TV_HFT_2023241.Client
 
         static void Create(string entity)
         {
-            if (entity == "Bookings")
+            try
             {
-                var bk = new Booking();
-                Console.WriteLine("What's the startdate?");
-                bk.StartDate = DateTime.Parse(Console.ReadLine());
-                Console.WriteLine("What's the enddate?");
-                bk.EndDate = DateTime.Parse(Console.ReadLine());
-                Console.WriteLine("Which table are you booking? [Id]");
-                bk.TableId = int.Parse(Console.ReadLine());
-                Console.WriteLine("Which customer is booking? [Id]");
-                bk.CustomerId = int.Parse(Console.ReadLine());
+                if (entity == "Bookings")
+                {
+                    var bk = new Booking();
+                    Console.WriteLine("What's the startdate?");
+                    bk.StartDate = DateTime.Parse(Console.ReadLine());
+                    Console.WriteLine("What's the enddate?");
+                    bk.EndDate = DateTime.Parse(Console.ReadLine());
+                    Console.WriteLine("Which table are you booking? [Id]");
+                    bk.TableId = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Which customer is booking? [Id]");
+                    bk.CustomerId = int.Parse(Console.ReadLine());
 
-                rest.Post(bk, "booking");
+                    rest.Post(bk, "booking");
+                }
+                else if (entity == "Customers")
+                {
+                    var cust = new Customer();
+                    Console.WriteLine("What's his/her name?");
+                    cust.Name = Console.ReadLine();
+                    Console.WriteLine("What's his/her phone number?");
+                    cust.Phone = Console.ReadLine();
+                    Console.WriteLine("What's his/her email?");
+                    cust.Email = Console.ReadLine();
+
+                    rest.Post(cust, "customer");
+                }
+                else if (entity == "PoolTables")
+                {
+                    var table = new PoolTable();
+                    Console.WriteLine("Is it a pool or a snooker table?");
+                    table.T_kind = Console.ReadLine();
+
+                    rest.Post(table, "pooltable");
+                }
             }
-            else if (entity == "Customers")
+            catch (Exception e)
             {
-                var cust = new Customer();
-                Console.WriteLine("What's his/her name?");
-                cust.Name = Console.ReadLine();
-                Console.WriteLine("What's his/her phone number?");
-                cust.Phone = Console.ReadLine();
-                Console.WriteLine("What's his/her email?");
-                cust.Email = Console.ReadLine();
-
-                rest.Post(cust, "customer");
+                Console.WriteLine(e.Message);
             }
-            else if (entity == "PoolTables")
-            {
-                var table = new PoolTable();
-                Console.WriteLine("Is it a pool or a snooker table?");
-                table.T_kind = Console.ReadLine();
+            finally { Console.ReadLine(); }
 
-                rest.Post(table, "pooltable");
-            }
         }
         static void List(string entity)
         {
@@ -79,133 +88,151 @@ namespace A3C6TV_HFT_2023241.Client
             Console.ReadLine();
         }
 
-        //update-nél a httprequest nem kap eredményt
+        //Update-nél a httprequest nem kap eredményt
         static void Update(string entity)
         {
-            if (entity == "Bookings")
+            try
             {
-                Console.Write("Give me an ID: ");
-                var ID = int.Parse(Console.ReadLine());
-                Booking bk = rest.Get<Booking>(ID, "booking");
-                Console.WriteLine(bk.ToString());
-                string answer = "";
-                while (answer != "no")
+                if (entity == "Bookings")
                 {
-                    Console.WriteLine();
-                    Console.WriteLine("What do you want to update? - [StartDate/EndDate/TableId/CustomerId]");
-                    Console.WriteLine("Type 'no', if you want to quit");
+                    Console.Write("Give me an ID: ");
+                    var ID = int.Parse(Console.ReadLine());
+                    Booking bk = rest.Get<Booking>(ID, "booking");
+                    Console.WriteLine(bk.ToString());
+                    string answer = "";
+                    while (answer != "no")
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("What do you want to update? - [StartDate/EndDate/TableId/CustomerId]");
+                        Console.WriteLine("Type 'no', if you want to quit");
+                        answer = Console.ReadLine().ToLower();
+                        switch (answer)
+                        {
+                            case "startdate":
+                                Console.Write("Enter a new StartDate: ");
+                                bk.StartDate = DateTime.Parse(Console.ReadLine());
+                                Console.WriteLine("StartDate updated!");
+                                break;
+                            case "enddate":
+                                Console.Write("Enter a new EndDate: ");
+                                bk.EndDate = DateTime.Parse(Console.ReadLine());
+                                Console.WriteLine("EndDate updated!");
+                                break;
+                            case "tableid":
+                                Console.Write("Enter a new TableId: ");
+                                bk.TableId = int.Parse(Console.ReadLine());
+                                Console.WriteLine("TableId updated!");
+                                break;
+                            case "customerid":
+                                Console.Write("Enter a new CustomerId: ");
+                                bk.CustomerId = int.Parse(Console.ReadLine());
+                                Console.WriteLine("CustomerId updated!");
+                                break;
+                        }
+                    }
+                    rest.Put(bk, "booking");
+                    Console.ReadLine();
+                }
+                else if (entity == "Customers")
+                {
+                    Console.Write("Give me an ID: ");
+                    var ID = int.Parse(Console.ReadLine());
+                    Customer cust = rest.Get<Customer>(ID, "customer");
+                    Console.WriteLine(cust.ToString());
+                    string answer = "";
+                    while (answer != "no")
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("What do you want to update? - [Name/Phone/Email]");
+                        Console.WriteLine("Type 'no', if you want to quit");
+                        answer = Console.ReadLine().ToLower();
+                        switch (answer)
+                        {
+                            case "name":
+                                Console.Write("Enter a new name: ");
+                                cust.Name = Console.ReadLine();
+                                Console.WriteLine("Name updated!");
+                                break;
+                            case "phone":
+                                Console.Write("Enter a new phone number: ");
+                                cust.Phone = Console.ReadLine();
+                                Console.WriteLine("Phone number updated!");
+                                break;
+                            case "email":
+                                Console.Write("Enter a new email: ");
+                                cust.Email = Console.ReadLine();
+                                Console.WriteLine("Email updated!");
+                                break;
+                        }
+                    }
+                    rest.Put(cust, "customer");
+                    Console.ReadLine();
+                }
+                else if (entity == "PoolTables")
+                {
+                    Console.Write("Give me an ID: ");
+                    var ID = int.Parse(Console.ReadLine());
+                    PoolTable table = rest.Get<PoolTable>(ID, "pooltable");
+                    Console.WriteLine(table.ToString());
+
+
+                    string answer = "";
+                    Console.WriteLine("Is it a pool or a snooker table?");
                     answer = Console.ReadLine().ToLower();
+
                     switch (answer)
                     {
-                        case "startdate":
-                            Console.Write("Enter a new StartDate: ");
-                            bk.StartDate = DateTime.Parse(Console.ReadLine());
-                            Console.WriteLine("StartDate updated!");
+                        case "pool":
+                            table.T_kind = "pool";
                             break;
-                        case "enddate":
-                            Console.Write("Enter a new EndDate: ");
-                            bk.EndDate = DateTime.Parse(Console.ReadLine());
-                            Console.WriteLine("EndDate updated!");
-                            break;
-                        case "tableid":
-                            Console.Write("Enter a new TableId: ");
-                            bk.TableId = int.Parse(Console.ReadLine());
-                            Console.WriteLine("TableId updated!");
-                            break;
-                        case "customerid":
-                            Console.Write("Enter a new CustomerId: ");
-                            bk.CustomerId = int.Parse(Console.ReadLine());
-                            Console.WriteLine("CustomerId updated!");
+                        case "snooker":
+                            table.T_kind = "snooker";
                             break;
                     }
+                    Console.WriteLine("Table updated!");
+
+                    rest.Put(table, "pooltable");
+                    Console.ReadLine();
                 }
-                rest.Put(bk, "booking");
-                Console.ReadLine();
             }
-            else if (entity == "Customers")
+            catch (Exception e)
             {
-                Console.Write("Give me an ID: ");
-                var ID = int.Parse(Console.ReadLine());
-                Customer cust = rest.Get<Customer>(ID, "customer");
-                Console.WriteLine(cust.ToString());
-                string answer = "";
-                while (answer != "no")
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("What do you want to update? - [Name/Phone/Email]");
-                    Console.WriteLine("Type 'no', if you want to quit");
-                    answer = Console.ReadLine().ToLower();
-                    switch (answer)
-                    {
-                        case "name":
-                            Console.Write("Enter a new name: ");
-                            cust.Name = Console.ReadLine();
-                            Console.WriteLine("Name updated!");
-                            break;
-                        case "phone":
-                            Console.Write("Enter a new phone number: ");
-                            cust.Phone = Console.ReadLine();
-                            Console.WriteLine("Phone number updated!");
-                            break;
-                        case "email":
-                            Console.Write("Enter a new email: ");
-                            cust.Email = Console.ReadLine();
-                            Console.WriteLine("Email updated!");
-                            break;
-                    }
-                }
-                rest.Put(cust, "customer");
-                Console.ReadLine();
+                Console.WriteLine(e.Message);
             }
-            else if (entity == "PoolTables")
-            {
-                Console.Write("Give me an ID: ");
-                var ID = int.Parse(Console.ReadLine());
-                PoolTable table = rest.Get<PoolTable>(ID, "pooltable");
-                Console.WriteLine(table.ToString());
+            finally { Console.ReadLine(); }
 
-
-                string answer = "";
-                Console.WriteLine("Is it a pool or a snooker table?");
-                answer = Console.ReadLine().ToLower();
-
-                switch (answer)
-                {
-                    case "pool":
-                        table.T_kind = "pool";
-                        break;
-                    case "snooker":
-                        table.T_kind = "snooker";
-                        break;
-                }
-                Console.WriteLine("Table updated!");
-
-                rest.Put(table, "customer");
-                Console.ReadLine();
-            }
         }
 
         //Delete-nél a httprequest nem kap eredményt
         static void Delete(string entity)
         {
-            if (entity == "Bookings")
+            try
             {
-                Console.Write("ID:");
-                int ID = int.Parse(Console.ReadLine());
-                rest.Delete(ID, "booking");
+                if (entity == "Bookings")
+                {
+                    Console.Write("ID:");
+                    int ID = int.Parse(Console.ReadLine());
+                    rest.Delete(ID, "booking");
+                }
+                else if (entity == "Customers")
+                {
+                    Console.Write("ID:");
+                    int ID = int.Parse(Console.ReadLine());
+                    rest.Delete(ID, "customer");
+                }
+                else if (entity == "PoolTables")
+                {
+                    Console.Write("ID:");
+                    int ID = int.Parse(Console.ReadLine());
+                    rest.Delete(ID, "pooltable");
+                }
             }
-            else if (entity == "Customers")
+            catch (Exception e)
             {
-                Console.Write("ID:");
-                int ID = int.Parse(Console.ReadLine());
-                rest.Delete(ID, "customers");
+                Console.WriteLine(e.Message);
             }
-            else if (entity == "PoolTables")
-            {
-                Console.Write("ID:");
-                int ID = int.Parse(Console.ReadLine());
-                rest.Delete(ID, "pooltables");
-            }
+            finally { Console.ReadLine(); }
+
         }
         static void Main(string[] args)
         {
@@ -240,7 +267,6 @@ namespace A3C6TV_HFT_2023241.Client
                 .Add("Exit", ConsoleMenu.Close);
 
             menu.Show();
-
         }
     }
 }
