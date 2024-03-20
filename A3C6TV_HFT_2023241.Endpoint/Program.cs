7fce0c9cd774b7dbc7180dc968e059b949212a72
Oláh Levente <levente@stud.uni-obuda.hu>
2024-03-20 23:50:38 +0100

    WPF customers example window added

--- A3C6TV_HFT_2023241.Endpoint/Program.cs
+++ A3C6TV_HFT_2023241.Endpoint/Program.cs
@@ -1,11 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace A3C6TV_HFT_2023241.Endpoint
 {
