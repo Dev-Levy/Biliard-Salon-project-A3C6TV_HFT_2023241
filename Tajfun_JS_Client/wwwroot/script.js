efe95dad350b56294169b34febd8b7877945b702
Oláh Levente <levente@stud.uni-obuda.hu>
2024-04-09 00:39:13 +0200

    gitignore

--- Tajfun_JS_Client/wwwroot/script.js
+++ Tajfun_JS_Client/wwwroot/script.js
@@ -4,24 +4,24 @@ let customerslist = [];
 let pooltableslist = [];
 let bookingslist = [];
 
-fetch('http://localhost:7332/customer')
-    .then(response => response.json())
-    .then(data => {
-        customerslist = data;
-        console.log(customerslist);
-    });
+//fetch('http://localhost:7332/customer')
+//    .then(response => response.json())
+//    .then(data => {
+//        customerslist = data;
+//        console.log(customerslist);
+//    });
 
-fetch('http://localhost:7332/pooltable')
-    .then(response => response.json())
-    .then(data => {
-        pooltableslist = data;
-        console.log(pooltableslist);
-    });
+//fetch('http://localhost:7332/pooltable')
+//    .then(response => response.json())
+//    .then(data => {
+//        pooltableslist = data;
+//        console.log(pooltableslist);
+//    });
 
-fetch('http://localhost:7332/booking')
-    .then(response => response.json())
-    .then(data => {
-        bookingslist = data;
-        console.log(bookingslist);
-    });
+//fetch('http://localhost:7332/booking')
+//    .then(response => response.json())
+//    .then(data => {
+//        bookingslist = data;
+//        console.log(bookingslist);
+//    });
 
