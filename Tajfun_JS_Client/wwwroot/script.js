d9dcedbf2f631833f80a7db43883a02a0ba4ed0a
Oláh Levente <levente@stud.uni-obuda.hu>
2024-04-09 18:31:56 +0200

    JS GET done

--- Tajfun_JS_Client/wwwroot/script.js
+++ Tajfun_JS_Client/wwwroot/script.js
@@ -1,27 +1,48 @@
 
+let customers = [];
+let pooltables = [];
+let bookings = [];
 
-let customerslist = [];
-let pooltableslist = [];
-let bookingslist = [];
+fetch('http://localhost:7332/customer')
+    .then(response => response.json())
+    .then(data => {
+        customers = data;
+        console.log(customers);
+        displayCustomers();
+    });
+fetch('http://localhost:7332/booking')
+    .then(response => response.json())
+    .then(data => {
+        bookings = data;
+        console.log(bookings);
+        displayBookings();
+    });
+fetch('http://localhost:7332/pooltable')
+    .then(response => response.json())
+    .then(data => {
+        pooltables = data;
+        console.log(pooltables);
+        displayPoolTables();
+    });
 
-//fetch('http://localhost:7332/customer')
-//    .then(response => response.json())
-//    .then(data => {
-//        customerslist = data;
-//        console.log(customerslist);
-//    });
 
-//fetch('http://localhost:7332/pooltable')
-//    .then(response => response.json())
-//    .then(data => {
-//        pooltableslist = data;
-//        console.log(pooltableslist);
-//    });
+function displayCustomers() {
+    customers.forEach(t => {
+        document.getElementById('customers').innerHTML +=
+            "<tr><td>" + t.name + "</td><td>" + t.phone + "</td><td>" + t.email + "</td></tr>";
+    })
+}
+function displayBookings() {
+    bookings.forEach(t => {
+        document.getElementById('bookings').innerHTML +=
+            "<tr><td>" + t.customer.name + "</td><td>" + t.startDate + "</td><td>" + t.endDate + "</td><td>" + t.poolTable.t_kind + "</td></tr>";
+    })
+}
 
-//fetch('http://localhost:7332/booking')
-//    .then(response => response.json())
-//    .then(data => {
-//        bookingslist = data;
-//        console.log(bookingslist);
-//    });
+function displayPoolTables() {
+    pooltables.forEach(t => {
+        document.getElementById('pooltables').innerHTML +=
+            "<tr><td>" + t.tableId + "</td><td>" + t.t_kind + "</td></tr>";
+    })
+}
 
