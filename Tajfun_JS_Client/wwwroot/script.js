4a73c789f51e82d927e1db45dca2fb89585cb7cb
Oláh Levente <levente@stud.uni-obuda.hu>
2024-04-10 14:52:27 +0200

    post UI done

--- Tajfun_JS_Client/wwwroot/script.js
+++ Tajfun_JS_Client/wwwroot/script.js
@@ -3,24 +3,53 @@ let customers = [];
 let pooltables = [];
 let bookings = [];
 
-fetch('http://localhost:7332/customer')
-    .then(response => response.json())
-    .then(data => {
-        customers = data;
-        console.log(customers);
-    });
-fetch('http://localhost:7332/booking')
-    .then(response => response.json())
-    .then(data => {
-        bookings = data;
-        console.log(bookings);
-    });
-fetch('http://localhost:7332/pooltable')
-    .then(response => response.json())
-    .then(data => {
-        pooltables = data;
-        console.log(pooltables);
-    });
+getCustomers();
+getBookings();
+getPoolTables();
+
+function formatDate(date) {
+    let d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear(),
+        hour = '' + d.getHours(),
+        minute = '' + d.getMinutes();
+
+    if (month.length < 2)
+        month = '0' + month;
+    if (day.length < 2)
+        day = '0' + day;
+    if (hour.length < 2)
+        hour = '0' + hour;
+    if (minute.length < 2)
+        minute = '0' + minute;
+
+    return [year, month, day].join('.') + ' ' + [hour, minute].join(':');
+}
+async function getCustomers() {
+    await fetch('http://localhost:7332/customer')
+        .then(response => response.json())
+        .then(data => {
+            customers = data;
+            console.log(customers);
+        });
+}
+async function getBookings() {
+   await fetch('http://localhost:7332/booking')
+        .then(response => response.json())
+        .then(data => {
+            bookings = data;
+            console.log(bookings);
+        });
+}
+async function getPoolTables() {
+   await fetch('http://localhost:7332/pooltable')
+        .then(response => response.json())
+        .then(data => {
+            pooltables = data;
+            console.log(pooltables);
+        });
+}
 
 
 function displayCustomers() {
@@ -42,10 +71,23 @@ function displayBookings() {
     document.getElementById('bookings').innerHTML = "";
     bookings.forEach(t => {
         document.getElementById('bookings').innerHTML +=
-            "<tr><td>" + t.customer.name + "</td><td>" + t.startDate + "</td><td>" + t.endDate + "</td><td>" + t.poolTable.t_kind + "</td></tr>";
+            "<tr><td>" + t.customer.name + "</td><td>" +
+           formatDate(t.startDate) + "</td><td>" + formatDate(t.endDate) +
+            "</td><td>" + t.poolTable.t_kind + "</td></tr>";
+    })
+
+    document.getElementById('customerSelect').innerHTML = "";
+    customers.forEach(c => {
+        document.getElementById('customerSelect').innerHTML +=
+            "<option value='" + c.customerId + "'>" + c.name + "</option>";
+    })
+
+    document.getElementById('poolTableSelect').innerHTML = "";
+    pooltables.forEach(t => {
+        document.getElementById('poolTableSelect').innerHTML +=
+            "<option value='" + t.tableId + "'> ID: " + t.tableId + "  Type: " + t.t_kind + "</option>";
     })
 }
-
 function displayPoolTables() {
     document.getElementById('bookingwindow').style.display = 'none'; // Hide bookingwindow
     document.getElementById('pooltablewindow').style.display = 'flex'; // Hide pooltablewindow
