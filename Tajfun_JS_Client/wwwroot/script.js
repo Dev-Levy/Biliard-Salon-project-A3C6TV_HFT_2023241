c46e87148b579b50c492a8f48b0fe1dbdf2cb73c
Oláh Levente <levente@stud.uni-obuda.hu>
2024-04-10 13:54:26 +0200

    js makeup

--- Tajfun_JS_Client/wwwroot/script.js
+++ Tajfun_JS_Client/wwwroot/script.js
@@ -8,31 +8,38 @@ fetch('http://localhost:7332/customer')
     .then(data => {
         customers = data;
         console.log(customers);
-        displayCustomers();
     });
 fetch('http://localhost:7332/booking')
     .then(response => response.json())
     .then(data => {
         bookings = data;
         console.log(bookings);
-        displayBookings();
     });
 fetch('http://localhost:7332/pooltable')
     .then(response => response.json())
     .then(data => {
         pooltables = data;
         console.log(pooltables);
-        displayPoolTables();
     });
 
 
 function displayCustomers() {
+    document.getElementById('bookingwindow').style.display = 'none'; // Hide bookingwindow
+    document.getElementById('pooltablewindow').style.display = 'none'; // Hide pooltablewindow
+    document.getElementById('customerwindow').style.display = 'flex'; // Show customerwindow
+
+    document.getElementById('customers').innerHTML = "";
     customers.forEach(t => {
         document.getElementById('customers').innerHTML +=
             "<tr><td>" + t.name + "</td><td>" + t.phone + "</td><td>" + t.email + "</td></tr>";
     })
 }
 function displayBookings() {
+    document.getElementById('bookingwindow').style.display = 'flex'; // Hide bookingwindow
+    document.getElementById('pooltablewindow').style.display = 'none'; // Hide pooltablewindow
+    document.getElementById('customerwindow').style.display = 'none'; // Show customerwindow
+
+    document.getElementById('bookings').innerHTML = "";
     bookings.forEach(t => {
         document.getElementById('bookings').innerHTML +=
             "<tr><td>" + t.customer.name + "</td><td>" + t.startDate + "</td><td>" + t.endDate + "</td><td>" + t.poolTable.t_kind + "</td></tr>";
@@ -40,6 +47,11 @@ function displayBookings() {
 }
 
 function displayPoolTables() {
+    document.getElementById('bookingwindow').style.display = 'none'; // Hide bookingwindow
+    document.getElementById('pooltablewindow').style.display = 'flex'; // Hide pooltablewindow
+    document.getElementById('customerwindow').style.display = 'none'; // Show customerwindow
+
+    document.getElementById('pooltables').innerHTML = "";
     pooltables.forEach(t => {
         document.getElementById('pooltables').innerHTML +=
             "<tr><td>" + t.tableId + "</td><td>" + t.t_kind + "</td></tr>";
